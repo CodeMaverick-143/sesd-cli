6aa22e2151ae3a7b1b8ea97cb921099c4e713c26
CodeMaverick-143 <arpit.01.sarang.2005@gmail.com>
2026-03-01 23:28:19 +0530

    fix the quote API link

--- src/commands/QuoteCommand.ts
+++ src/commands/QuoteCommand.ts
@@ -2,14 +2,15 @@ import { BaseCommand } from "./BaseCommand";
 import { ApiService } from "../services/ApiService";
 import { Logger } from "../utils/Logger";
 
-export class QuoteCommand extends BaseCommand{
+export class QuoteCommand extends BaseCommand {
     private api = new ApiService();
 
-    async execute(): Promise<void>{
-        const data = await this.api.get("https://api.quotable.io/random")
+    async execute(): Promise<void> {
+        const data = await this.api.get("https://zenquotes.io/api/random")
+        const quote = data[0];
 
-    Logger.success(`"${data.content}"`);
-    Logger.info(`- ${data.author}`);
+        Logger.success(`"${quote.q}"`);
+        Logger.info(`- ${quote.a}`);
     }
 }
 
